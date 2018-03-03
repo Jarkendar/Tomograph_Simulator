b2566ed7a5d71d760368610886d720ed83016581
jaroslaw <jarek.skrzypczak@o2.pl>
2018-03-03 19:12:40 +0100

    Initial commit

---
+++ src/main/java/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+    public ImageView inputImage;
+    public ImageView sinogramImage;
+    public ImageView outputImage;
+    public ImageView stepImage;
+    public Button chooseFileButton;
+    public TextField detectorNumberTextField;
+    public TextField degreesRangeTextField;
+    public TextField measureNumberTextField;
+    public Slider stepSlider;
+
+}
