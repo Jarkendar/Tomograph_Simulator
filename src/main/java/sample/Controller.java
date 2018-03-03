5f08685befbfc24916e9bc290bf162ab74e0e3c0
jaroslaw <jarek.skrzypczak@o2.pl>
2018-03-03 20:34:53 +0100

    Add read file Change all link to java 1.5 to java 1.8

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,10 +1,20 @@
 package sample;
 
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Controller {
     public ImageView inputImage;
@@ -18,4 +28,64 @@ public class Controller {
     public Slider stepSlider;
     public CheckBox filteringCheckBox;
     public Button transformButton;
+
+    private File file;
+    private String fileExtension;
+    private Image originalImage;
+
+    public void clickChooseFile(ActionEvent actionEvent) {
+        actionEvent.getSource();
+        file = Main.openFileChooser();
+
+        if (file != null) {
+            fileExtension = getFileExtension(file);
+            switch (fileExtension){
+                case "DICOM":{
+                    //todo do something
+                    break;
+                }
+                case "png":{
+                    readImage(file);
+                }
+                case "jpg":{
+                    readImage(file);
+                }
+            }
+            transformButton.setDisable(false);
+        }else {
+            transformButton.setDisable(true);
+        }
+    }
+
+    private void readImage(File file){
+        try {
+            BufferedImage img = ImageIO.read(file);
+            int width = img.getWidth();
+            int height = img.getHeight();
+            int[][] imgArr = new int[width][height];
+            Raster raster = img.getData();
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    imgArr[i][j] = raster.getSample(i, j, 0);
+                }
+            }
+            inputImage.setImage(castBufferedImageToImage(img));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private Image castBufferedImageToImage(BufferedImage bufferedImage){
+        return SwingFXUtils.toFXImage(bufferedImage, null);
+    }
+
+    private String getFileExtension(File file) {
+        String name = file.getName();
+        try {
+            return name.substring(name.lastIndexOf(".") + 1);
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,16 +4,31 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
+    private static final FileChooser fileChooser = new FileChooser();
+    private static Stage stage;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Tomograph_Simulator");
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
+    }
+
+    public static File openFileChooser() {
+        fileChooser.setTitle("Choose file:");
+
+        File file = fileChooser.showOpenDialog(stage);
+        System.out.println(file);
+        return file;
     }
 
 
